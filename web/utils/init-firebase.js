78cbebcdf28deaa3f5f3b4143c1a9060d79550b7
Ben Winding <hello@benwinding.com>
2021-08-31 21:29:11 +1000

    Init nextjs

--- web/utils/init-firebase.js
+++ web/utils/init-firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBfsrg8h4ySA0m7OA5dNyexzyGHpTzQZU8",
     authDomain: "brij-miranda-wedding.firebaseapp.com",
@@ -6,7 +9,8 @@ const firebaseConfig = {
     messagingSenderId: "680052154962",
     appId: "1:680052154962:web:0c6fca9a2cf7431375358b"
 };
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const DB = app.firestore();
+export const DB = firebase.firestore(); 
