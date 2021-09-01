6d6c4488464e0ab28c13183a923463b713959492
Ben Winding <hello@benwinding.com>
2021-09-01 22:28:04 +1000

    Guest repsonses working well

--- web/pages/rsvp.js
+++ web/pages/rsvp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ButtonToggleField, SelectField, TextAreaField } from '../shared/fields';
 import { DB } from '../utils/init-firebase';
 
 export default function Page() {
@@ -11,32 +12,103 @@ export default function Page() {
         const params = Object.fromEntries(urlSearchParams.entries());
         const guestId = params.id;
         setGuestId(guestId);
+    }, []);
 
+    React.useEffect(() => {
         DB.collection('guests').doc(guestId).get()
             .then(res => {
+                console.log('retrieved guest: ', {res, exists: res.exists, data: res.data(), guestId});
                 if (!res.exists) {
-                    setStatus('not-found');
+                    setStatus(`Guest with id=${guestId} was not found!`);
                 } else {
                     setStatus('loaded');
                     setGuestDoc(res.data())
                 }
-            });
-    }, []);
+            })
+            .catch(err => setStatus(err.toString()))
+    }, [guestId])
+
+    const onSubmit = (result) => {
+        console.log('updating response: ', {result, guestId});
+        DB.collection('guest-responses').doc(guestId).set(result, {merge: true});
+    }
 
     if (status === 'loading') {
         return <h1>Loading</h1>
     }
-    if (status === 'not-found') {
+    if (status !== 'loaded' || !guestDoc) {
         return <h1>Guest '{guestId}' Not Found</h1>
     }
 
     return (
-        <div>
-            <h1>RSVP</h1>
-            <h2>Name: {guestDoc && guestDoc.first_name}</h2>
-            <h2>Images: (Coming Soon)</h2>
-        </div>
+        <RsvpStateless
+            firstName={guestDoc.first_name}
+            lastName={guestDoc.last_name}
+            address={guestDoc.address}
+            onSubmit={onSubmit}
+        />
     )
+}
+
+const dietaryOptions = [
+    { label: 'None', value: 'none' },
+    { label: 'Vegan', value: 'vegan' },
+    { label: 'Gluten Free', value: 'gf' },
+    { label: 'Vegetarian', value: 'vegetarian' },
+    { label: 'Dairy Free', value: 'dairy-free' },
+    { label: 'Other', value: 'other' },
+]
+
+const areYouComingOptions = [
+    { label: 'Yes', value: 'yes' },
+    { label: 'Sorry No', value: 'no' },
+]
+
+function RsvpStateless({ firstName, lastName, address, onSubmit }) {
+    const [dietOption, setDietOption] = React.useState('none');
+    const [extraDietInfo, setExtraDietOption] = React.useState('');
+    const [areYouComingResult, setAreYouComingResult] = React.useState('');
+    const isComing = areYouComingResult === 'yes';
+    const notComing = areYouComingResult === 'no';
+    const isDietNone = dietOption === 'none';
+
+    const onClickedSubmit = () => {
+        const resultObj = {
+            is_coming: isComing,
+            diet_option: dietOption,
+            diet_extra_info: extraDietInfo,
+            created_at: new Date().toISOString()
+        }
+        onSubmit(resultObj);
+    }
+
+    return <div className="w-full min-h-screen bg-base-200">
+        <div className="mx-auto max-w-md flex flex-col align-center text-center">
+            <p className="text-3xl font-bold my-4">RSVP</p>
+            <p className="text-5xl">Dear {firstName} {lastName}</p>
+            <p className="mb-5">You've been invited to the wedding of Brij and Miranda Daniel</p>
+
+            <table className="table w-full table-compact my-4">
+                <tbody>
+                    <tr><td>Name</td><td>{firstName} {lastName}</td></tr>
+                    <tr><td>Address</td><td>{address}</td></tr>
+                </tbody>
+            </table>
+
+            <h1 className="mt-5 mb-3">Your Response</h1>
+            <div>
+                <ButtonToggleField label="Are you coming?" options={areYouComingOptions} onChange={setAreYouComingResult} />
+                {isComing && <>
+                    <SelectField label="Dietary Requirements?" options={dietaryOptions} onChange={setDietOption} />
+                    {!isDietNone && <TextAreaField label="Any extra dietary information?" onChange={setExtraDietOption} />}
+                </>}
+                {notComing && <>
+                    <h2 className="mt-3">We're sorry to hear that, look forward to seeing you soon :)</h2>
+                </>}
+            </div>
+            <button disabled={!areYouComingResult} onClick={onClickedSubmit} className="my-3 btn btn-primary">Send</button>
+        </div>
+    </div>
 }
 
 Page.isPublic = true;
--- web/shared/fields.js
+++ web/shared/fields.js
@@ -1,26 +1,69 @@
-export function TextField(props) {
+import React from 'react';
+
+export function TextField({ label, onChange }) {
     return <div className="form-control">
         <label className="label">
-            <span className="label-text">{props.label}</span>
+            <span className="label-text">{label}</span>
         </label>
-        <input onChange={e => props.onChange(e.currentTarget.value)} type="text" placeholder={props.label} className="input input-bordered" />
+        <input onChange={e => onChange(e.currentTarget.value)} type="text" placeholder={label} className="input input-bordered" />
     </div>;
 }
 
-export function PasswordField(props) {
+export function PasswordField({ label, onChange }) {
     return <div className="form-control">
         <label className="label">
-            <span className="label-text">{props.label}</span>
+            <span className="label-text">{label}</span>
         </label>
-        <input onChange={e => props.onChange(e.currentTarget.value)} type="password" placeholder={props.label} className="input input-bordered" />
+        <input onChange={e => onChange(e.currentTarget.value)} type="password" placeholder={label} className="input input-bordered" />
     </div>;
 }
 
-export function TextAreaField(props) {
+export function TextAreaField({ label, onChange }) {
     return <div className="form-control">
         <label className="label">
-            <span className="label-text">{props.label}</span>
+            <span className="label-text">{label}</span>
         </label>
-        <textarea rows={4} onChange={e => props.onChange(e.currentTarget.value)} type="text" placeholder={props.label} className="textarea textarea-bordered" />
+        <textarea rows={4} onChange={e => onChange(e.currentTarget.value)} type="text" placeholder={label} className="textarea textarea-bordered" />
+    </div>;
+}
+
+export function CheckboxField({ label, onChange }) {
+    return <div className="p-6 card bordered">
+        <div className="form-control">
+            <label className="cursor-pointer label">
+                <span className="label-text">{label}</span>
+                <input type="checkbox" onChange={e => onChange(e.currentTarget.value)} className="checkbox" />
+            </label>
+        </div>
+    </div>;
+}
+
+export function ButtonToggleField({ label, options, onChange }) {
+    const [selected, setSelected] = React.useState();
+    React.useEffect(() => onChange(selected), [selected]);
+
+    return <div className="form-control">
+        <label className="label">
+            <span className="label-text">{label}</span>
+        </label>
+        <div className="btn-group">
+            {options.map((o, i) => (<button
+                key={i}
+                className={'btn btn-outline btn-lg ' + (o.value === selected ? 'btn-active' : '')}
+                onClick={() => setSelected(o.value)} 
+                value={o.value}
+            >{o.label}</button>))}
+        </div>
+    </div>;
+}
+
+export function SelectField({ label, options, onChange }) {
+    return <div className="form-control">
+        <label className="label">
+            <span className="label-text">{label}</span>
+        </label>
+        <select onChange={e => onChange(e.currentTarget.value)} className="select select-bordered select-lg w-full max-w-xs">
+            {options.map((o, i) => (<option key={i} value={o.value}>{o.label}</option>))}
+        </select>
     </div>;
 }
