956952b5731779ee168c2033d9c44f09804200f4
Ben Winding <hello@benwinding.com>
2021-09-02 00:23:02 +1000

    Added 404 page

---
+++ web/pages/404.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="text-center hero-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-8xl font-bold">
+            404 Error
+          </h1>
+          <a href="/"><h1 className="mb-5 text-5xl font-bold">
+            Brij {'&'} Miranda's Wedding
+          </h1></a>
+          <p className="mb-5">
+            This website is to commemorate the sacred union of Brij and Miranda :)
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Page.isPublic = true;
