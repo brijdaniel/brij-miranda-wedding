f5c7e0b1f5ade797829598182145ebeca0e798fc
Ben Winding <hello@benwinding.com>
2021-09-08 19:18:54 +1000

    Refactor QR to component

--- web/pages/rsvp.js
+++ web/pages/rsvp.js
@@ -3,7 +3,7 @@ import { ButtonToggleField, SelectField, TextAreaField } from '../shared/fields'
 import { LoadingScreen } from '../shared/loading-screen';
 import { ErrorScreen } from '../shared/error-screen';
 import { DB } from '../utils/init-firebase';
-import { toDataURL } from 'qrcode'
+import { QrCodeImage } from '../shared/qr-code-image';
 
 export default function Page() {
     const [guestDoc, setGuestDoc] = React.useState();
@@ -38,21 +38,39 @@ export default function Page() {
         DB.collection('guest-responses').doc(guestId).set(result, { merge: true });
     }
 
-    if (status === 'error') {
-        return <ErrorScreen label={statusDetail} />
-    }
-    if (status === 'loading') {
+    if (status === 'loading' || !guestDoc) {
         return <LoadingScreen label="Finding guest information" />
     }
 
+    if (status === 'error') {
+        return <ErrorScreen label={statusDetail} />
+    }
+
+    const firstName = guestDoc.first_name;
+    const lastName = guestDoc.last_name;
+    const address = guestDoc.address;
+
     return (
-        <RsvpStateless
-            guestId={guestId}
-            firstName={guestDoc.first_name}
-            lastName={guestDoc.last_name}
-            address={guestDoc.address}
-            onSubmit={onSubmit}
-        />
+        <div className="w-full min-h-screen bg-base-200">
+            <div className="mx-auto max-w-md px-3 flex flex-col align-center text-center">
+                <p className="text-3xl font-bold my-4">RSVP</p>
+                <p className="text-5xl">Dear {firstName} {lastName}</p>
+                <p className="mb-5">You've been invited to the wedding of Brij Daniel and Miranda Green</p>
+
+                <table className="table w-full table-compact my-4">
+                    <tbody>
+                        <tr><td>Name</td><td>{firstName} {lastName}</td></tr>
+                        <tr><td>Address</td><td>{address}</td></tr>
+                    </tbody>
+                </table>
+
+                <h1 className="mt-5 mb-3">Your Response</h1>
+                <RsvpForm
+                    guestId={guestId}
+                    onSubmit={onSubmit}
+                />
+            </div>
+        </div>
     )
 }
 
@@ -80,9 +98,7 @@ const transportLocationOptions = [
     { label: 'CBD', value: 'CBD' },
 ]
 
-function RsvpStateless({ guestId, firstName, lastName, address, onSubmit }) {
-    const [qrImgUrl, setQrImgUrl] = React.useState('none');
-
+function RsvpForm({ guestId, onSubmit }) {
     const [dietOption, setDietOption] = React.useState('none');
     const [extraDietInfo, setExtraDietOption] = React.useState('');
     const [areYouComingResult, setAreYouComingResult] = React.useState('');
@@ -93,16 +109,6 @@ function RsvpStateless({ guestId, firstName, lastName, address, onSubmit }) {
     const isDietNone = dietOption === 'none';
     const isTransportNone = wouldYouLikeTransportResult === 'no';
     const isTransportRequired = !isTransportNone;
-
-    React.useEffect(() => {
-        if (!guestId || guestId == 'loading') {
-            return;
-        }
-        const rsvpUrl = `${location.origin}/rsvp?id=${guestId}`
-        toDataURL(rsvpUrl, function (err, dataUrl) {
-            setQrImgUrl(dataUrl);
-        });
-    }, [guestId])
 
     const onClickedSubmit = () => {
         const resultObj = {
@@ -116,39 +122,24 @@ function RsvpStateless({ guestId, firstName, lastName, address, onSubmit }) {
         onSubmit(resultObj);
     }
 
-    return <div className="w-full min-h-screen bg-base-200">
-        <div className="mx-auto max-w-md px-3 flex flex-col align-center text-center">
-            <p className="text-3xl font-bold my-4">RSVP</p>
-            <p className="text-5xl">Dear {firstName} {lastName}</p>
-            <p className="mb-5">You've been invited to the wedding of Brij Daniel and Miranda Green</p>
-
-            <table className="table w-full table-compact my-4">
-                <tbody>
-                    <tr><td>Name</td><td>{firstName} {lastName}</td></tr>
-                    <tr><td>Address</td><td>{address}</td></tr>
-                </tbody>
-            </table>
-
-            <h1 className="mt-5 mb-3">Your Response</h1>
-            <div>
-                <ButtonToggleField label="Are you coming?" options={areYouComingOptions} onChange={setAreYouComingResult} />
-                {isComing && <>
-                    <SelectField label="Dietary Requirements?" options={dietaryOptions} onChange={setDietOption} />
-                    {!isDietNone && <TextAreaField label="Any extra dietary information?" onChange={setExtraDietOption} />}
-                    <SelectField initialValue="no" label="Would you like transport?" options={wouldYouLikeTransportOptions} onChange={setWouldYouLikeTransportResult} />
-                    {!isTransportNone && <SelectField label="Where from?" options={transportLocationOptions} onChange={setTransportLocationResult} />}
-                </>}
-                {isNotComing && <>
-                    <h2 className="mt-3">We're sorry to hear that, look forward to seeing you soon :)</h2>
-                </>}
-            </div>
-            <button disabled={!areYouComingResult} onClick={onClickedSubmit} className="my-3 btn btn-primary">Send Response</button>
-            {qrImgUrl && <div className="items-center flex flex-col max-w-full">
-                <h4 className="mt-3 -mb-5 z-10">Guest QR Code</h4>
-                <img width={200} src={qrImgUrl} />
-            </div>}
-        </div>
+    return <div>
+        <ButtonToggleField label="Are you coming?" options={areYouComingOptions} onChange={setAreYouComingResult} />
+        {isComing && <>
+            <SelectField label="Dietary Requirements?" options={dietaryOptions} onChange={setDietOption} />
+            {!isDietNone && <TextAreaField label="Any extra dietary information?" onChange={setExtraDietOption} />}
+            <SelectField initialValue="no" label="Would you like transport?" options={wouldYouLikeTransportOptions} onChange={setWouldYouLikeTransportResult} />
+            {!isTransportNone && <SelectField label="Where from?" options={transportLocationOptions} onChange={setTransportLocationResult} />}
+        </>}
+        {isNotComing && <>
+            <h2 className="mt-3">We're sorry to hear that, look forward to seeing you soon :)</h2>
+        </>}
+        <button disabled={!areYouComingResult} onClick={onClickedSubmit} className="my-3 btn btn-primary">Send Response</button>
+        {guestId && <div className="items-center flex flex-col max-w-full">
+            <h4 className="mt-3 -mb-5 z-10">Guest QR Code</h4>
+            <QrCodeImage url={`${location.origin}/rsvp?id=${guestId}`}  />
+        </div>}
     </div>
+
 }
 
 Page.isPublic = true;
---
+++ web/shared/qr-code-image.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { toDataURL } from 'qrcode'
+
+export function QrCodeImage({ url }) {
+    const [qrImgUrl, setQrImgUrl] = React.useState('none');
+
+    React.useEffect(() => {
+        if (!url) {
+            return;
+        }
+        toDataURL(url, function (err, dataUrl) {
+            setQrImgUrl(dataUrl);
+        });
+    }, [url])
+
+    return qrImgUrl && <img width={200} src={qrImgUrl} />
+}
