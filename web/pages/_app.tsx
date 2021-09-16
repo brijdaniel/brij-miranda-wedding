9ba32e4db7ca1e9b3f1c019ca964355768da88fd
Brij <brij.daniel@gmail.com>
2021-09-16 18:32:27 +0930

    Made info page public, throws error

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,7 +2,7 @@ import { LoginWrapper } from '../shared/login-wrapper';
 import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
-const publicPages = ['/', '/rsvp', '/404']
+const publicPages = ['/', '/rsvp', '/404', '/info']
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
