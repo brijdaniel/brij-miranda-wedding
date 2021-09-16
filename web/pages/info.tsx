e2b9ad691742b8bebd5bb2e787abc9ae35a15633
Brij <brij.daniel@gmail.com>
2021-09-16 19:15:09 +0930

    Still having type error on info page

--- web/pages/info.tsx
+++ web/pages/info.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Page() {
-
+    const styleOne = {position: "relative", height: "0", padding-top: "140.9524%", padding-bottom: "48px", box-shadow: "0 2px 8px 0 rgba(63,69,81,0.16)", margin-top: "1.6em", margin-bottom: "0.9em", overflow: "hidden", border-radius: "8px", will-change: "transform"};
     // Below is a Canva embed link
     return (
-        <div className="canva-embed">
-            <div style="position: relative; width: 100%; height: 0; padding-top: 140.9524%; padding-bottom: 48px; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden; border-radius: 8px; will-change: transform;">
+        <div>
+            <div style={styleOne}>
                 <iframe loading="lazy" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;" src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEpHiasgjg&#x2F;view?embed">
                 </iframe>
             </div>
@@ -13,5 +13,3 @@ export default function Page() {
         </div>
     )
 }
-
-// Page.isPublic = true;
