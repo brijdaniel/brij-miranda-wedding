94a7a3c0a269204788d4d2256a36a7c063744818
Brij <brij.daniel@gmail.com>
2021-09-23 20:45:54 +0930

    trying to update invitation page, not working

--- web/pages/guests/invitations/[id].tsx
+++ web/pages/guests/invitations/[id].tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { Invitation } from "shared/invitation";
+import { DB } from '../utils/init-firebase';
+import { Family } from 'shared/guest.model';
+
+
 
 export default function Page() {
+    const [guestDoc, setGuestDoc] = React.useState<Family>();
+    const [status, setStatus] = React.useState('loading');
+    const [guestId, setGuestId] = React.useState<string>();
+
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if (!router.isReady) {
+          return;
+        }
+        const guestId = router.query.id as string;
+        setGuestId(guestId);
+      }, [router]);
+
+    React.useEffect(() => {
+        if (!guestId) {
+          return;
+        }
+        DB.collection('families').doc(guestId).get()
+          .then(res => {
+            console.log('retrieved guest: ', { res, exists: res.exists, data: res.data(), guestId });
+            if (!res.exists) {
+              setStatus('error')
+            } else {
+              setGuestDoc(res.data() as Family)
+              setStatus('loaded');
+            }
+          })
+          .catch(err => setStatus(err.toString()))
+      }, [guestId])
+
+    const familyName = guestDoc.family_name;
+
     return <>
-        <Invitation id="" family_name="The Family" />
+        <Invitation id={guestId} family_name={familyName} />
     </>;
 }
