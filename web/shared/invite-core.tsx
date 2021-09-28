1ffabf0791316aba6caeef17595f717cceeb04e5
Brij <brij.daniel@gmail.com>
2021-09-28 18:29:11 +0930

    added file

---
+++ web/shared/invite-core.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function InviteCore() {
+    return <div>
+        <div className="transform -skew-y-12 text-6xl font-pinyon-script flex flex-col items-center">
+        <p className="">Brij</p>
+        <p className="font-allison mt-3">{'&'}</p>
+        <p className="text-7xl">Miranda</p>
+        </div>
+        <div className="w-full flex flex-col items-end mt-24">
+        <div className="font-serif flex flex-col gap-3 items-start mr-12">
+            <p className="text-2xl">5th March, 2022</p>
+            <p className="">4:30pm Ceremony, Reception to follow</p>
+            <p className="">Maximilians, Verdun SA</p>
+            <p className="">RSVP by: 5th January, 2022</p>
+        </div>
+        </div>
+    </div>
+}
