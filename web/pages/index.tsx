a618e33451075a6f2bab8d64c5c56e425323c103
Brij <brij.daniel@gmail.com>
2021-10-09 18:57:13 +1030

    added click the map text

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -24,8 +24,9 @@ export default function Page() {
         </div>
       </div>
       <div className="mt-10" >
+      <p className="text-center">&#8595;&#8595;&#8595; Click the map &#8595;&#8595;&#8595;</p>
         <a href="https://www.google.com/maps/place/Maximilians+Restaurant/@-34.95585,138.619768,11.81z/data=!4m5!3m4!1s0x0:0x3a95c76dc2f4028a!8m2!3d-35.0109219!4d138.7839548">
-          <img className="p-10" src="/map.JPG"/>
+          <img className="px-10 pb-10" src="/map.JPG"/>
         </a>
       </div>
     </div>
