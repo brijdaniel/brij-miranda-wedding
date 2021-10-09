68492ebdf57a109797fd0de6d8d0298c38c3d4b3
Brij <brij.daniel@gmail.com>
2021-10-09 19:13:45 +1030

    removed admin button from home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,9 +4,6 @@ import { InviteCore } from "../shared/invite-core";
 
 export default function Page() {
   return <div className="hero min-h-screen bg-base-200 bg-gray-200 bg-flowers">
-    <div className="absolute right-0 top-0">
-      <Link href="/guests"><a className="btn bg-red-500 opacity-30 m-3">Admin</a></Link>
-    </div>
     <div className="relative overflow-y-auto pt-20">
       <InviteCore />      
       <div className="transform text-6xl font-pinyon-script flex flex-col items-center">
