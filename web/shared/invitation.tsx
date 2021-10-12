f2eb74f16cece9a28126bb4b8343a4148ea205e2
Ben Winding <hello@benwinding.com>
2021-10-12 18:00:52 +1100

    Front and back of invite implemented

--- web/shared/invitation.tsx
+++ web/shared/invitation.tsx
@@ -6,25 +6,38 @@ interface InvitationProps {
   family_name: string;
   id: string;
 }
+const width = '148mm';
+const height = '210mm';
 
-export function Invitation({ family_name, id }: InvitationProps) {
-  const width = '148mm';
-  const height = '210mm';
+export function Invitation(props: InvitationProps) {
+  return <>
+    <InvitationFront {...props} />
+    <InvitationBack {...props} />
+  </>;
+}
 
-  return <div className="page-break-avoid bg-gray-200 bg-flowers border" style={{width, height}}>
-    <div className="transform mt-8 mb-16 font-pinyon-script text-center flex flex-col items-center gap-8">
+function InvitationFront({ family_name, id }: InvitationProps) {
+  return <div className="page-break-avoid bg-gray-200 bg-flowers border flex flex-col justify-center" style={{ width, height }}>
+    <div className="transform mb-16 font-pinyon-script text-center flex flex-col items-center gap-8">
       <div className="text-5xl">{family_name}</div>
-      <p className="font-serif">Please join us to celebrate the wedding of</p>    
+      <p className="font-serif">Please join us to celebrate the wedding of</p>
     </div>
     <div>
       <InviteCore />
     </div>
-    
-    <div className="items-center flex flex-col w-full mt-4">
-      <div className="relative">
-        <h4 className="z-10 -left-24 bottom-12 absolute text-gray-200">{'RSVP here! ->'}</h4>
-        <QrCodeImage url={`${location.origin}/rsvp?id=${id}`} />
-      </div>
+  </div>;
+}
+
+function InvitationBack({ family_name, id }: InvitationProps) {
+  const isLocalHost = location.origin.includes('localhost');
+  return <div className="page-break-avoid bg-gray-200 bg-flowers border flex flex-col justify-center" style={{ width, height }}>
+    <div className="transform mt-16 mb-4 font-pinyon-script text-center flex flex-col items-center gap-8">
+      <div className="text-3xl">{family_name}</div>
+    </div>
+    <div className="items-center flex flex-col w-full mt-10 gap-2 mb-10">
+      <div className="text-xs">↓↓↓ RSVP link ↓↓↓</div>
+      <QrCodeImage url={`${location.origin}/rsvp?id=${id}`} />
+      {isLocalHost && <h4 className="text-red-500 font-bold text-3xl">WARNING THIS IS LOCALHOST</h4>}
     </div>
   </div>;
 }
