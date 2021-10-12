1f1755bd4e1631543affa9b67ec75b32f4aa8585
Ben Winding <hello@benwinding.com>
2021-10-12 18:43:33 +1100

    Add phone link

--- web/shared/invite-details.tsx
+++ web/shared/invite-details.tsx
@@ -1,4 +1,5 @@
 export function InviteDetails() {
+  const PHONE = '0437090088';
   return <>
     <div className="transform text-6xl font-pinyon-script flex flex-col items-center">
       <div className="items-end mt-20">
@@ -9,7 +10,7 @@ export function InviteDetails() {
       <div className="font-serif flex flex-col items-center gap-3 text-center">
         <p className="">15 Onkaparinga Valley Rd, Verdun SA 5245</p>
         <p className="">Bus transportation will be available, please let us know your preferences in your RSVP!</p>
-        <p className="">If you have any questions, or would like recommendations for accomodation, please contact Miranda on 0437090088</p>
+        <p className="">If you have any questions, or would like recommendations for accomodation, please contact Miranda on <a className="underline text-blue-600" href={"tel:" + PHONE}>{PHONE}</a></p>
         <p className="">Our Wedding is an 18+ event</p>
         <p className="">Thank you for your understanding</p>
       </div>
