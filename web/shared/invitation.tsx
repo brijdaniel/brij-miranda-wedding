6cc55fe684ed09e880c405d7479c203e3cf38f55
Ben Winding <hello@benwinding.com>
2021-10-12 17:42:14 +1100

    Set size, layout changes

--- web/shared/invitation.tsx
+++ web/shared/invitation.tsx
@@ -8,21 +8,19 @@ interface InvitationProps {
 }
 
 export function Invitation({ family_name, id }: InvitationProps) {
-  const width = '500px';
-  const height = '800px';
+  const width = '148mm';
+  const height = '210mm';
 
   return <div className="page-break-avoid bg-gray-200 bg-flowers border" style={{width, height}}>
-    <div className="transform my-8 font-pinyon-script text-center">
+    <div className="transform mt-8 mb-16 font-pinyon-script text-center flex flex-col items-center gap-8">
       <div className="text-5xl">{family_name}</div>
-      <div className="font-serif flex flex-col gap-3 items-start mr-12">
-        <p className="">Please join us to celebrate the wedding of</p>    
-    </div>
+      <p className="font-serif">Please join us to celebrate the wedding of</p>    
     </div>
     <div>
       <InviteCore />
     </div>
     
-    <div className="items-center flex flex-col w-full">
+    <div className="items-center flex flex-col w-full mt-4">
       <div className="relative">
         <h4 className="z-10 -left-24 bottom-12 absolute text-gray-200">{'RSVP here! ->'}</h4>
         <QrCodeImage url={`${location.origin}/rsvp?id=${id}`} />
--- web/shared/invite-core.tsx
+++ web/shared/invite-core.tsx
@@ -3,12 +3,12 @@ import React from "react";
 export function InviteCore() {
     return <div>
         <div className="transform -skew-y-12 text-6xl font-pinyon-script flex flex-col items-center">
-        <p className="">Brij</p>
-        <p className="font-allison mt-3">{'&'}</p>
-        <p className="text-7xl">Miranda</p>
+            <p className="">Brij</p>
+            <p className="font-allison mt-3">{'&'}</p>
+            <p className="text-7xl">Miranda</p>
         </div>
-        <div className="w-full flex flex-col items-center mt-24">
-        <div className="font-serif flex flex-col gap-3 items-center">
+        <div className="w-full flex flex-col items-center mt-16">
+        <div className="font-serif flex flex-col gap-2 items-center">
             <p className="text-2xl">March 5th, 2022</p>
             <p className="">4pm Arrival for a 4:30pm Ceremony</p>
             <p className="">Reception to follow</p>
